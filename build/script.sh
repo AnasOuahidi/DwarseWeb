7484640659283839cc6a6136a7e8ad2ce84abf2b
akagami <anasouahidi@gmail.com>
2017-02-15 16:07:31 +0100

    [aouahidi]: Mise a jour de la page d'authentification

--- build/script.sh
+++ build/script.sh
@@ -3,6 +3,6 @@ cd assets/
 git init
 git add --all
 git commit -m "Commit"
-git remote add origin https://github.com/dwarse/dwarse.github.io.git
+git remote add origin https://dwarse:dwarse123@github.com/dwarse/dwarse.github.io.git
 git push -u origin master
 cd ../
