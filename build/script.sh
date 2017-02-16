8a9b0e53e32011ce9e1652b84089b0b1f85c4f6b
akagami <anasouahidi@gmail.com>
2017-02-16 10:10:53 +0100

    [aouahidi]: Configuration de la generation de pages

--- build/script.sh
+++ build/script.sh
@@ -6,4 +6,5 @@ git commit -m "Commit"
 git remote add origin https://dwarse:dwarse123@github.com/dwarse/dwarse.github.io.git
 git push -u origin master --force
 cd ../
+git add .
 xdg-open https://dwarse.github.io/
