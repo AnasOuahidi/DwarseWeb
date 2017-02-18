f8021d80dbca72624aa40c78f377aadcc79f17e6
akagami <anasouahidi@gmail.com>
2017-02-18 20:34:03 +0100

    [aouahidi]: Ajout de la page de confirmation

--- pages/confirm/confirmCtrl.js
+++ pages/confirm/confirmCtrl.js
@@ -2,10 +2,8 @@ export let confirmCtrl = ['$scope', '$state', '$stateParams', '$http', 'Factory'
     $('title').html('Confirmation du compte')
     $('body').addClass('bg')
     $('nav').hide()
-    $('.inner-container').height('22%')
     $scope.showLoader = true
     if (!$stateParams.token || $stateParams.token.length !== 64) {
-        $('.inner-container').height('25%')
         $scope.showLoader = false
         $scope.tokenError = true
     } else {
@@ -13,7 +11,6 @@ export let confirmCtrl = ['$scope', '$state', '$stateParams', '$http', 'Factory'
             $scope.showLoader = false
             $scope.withSuccess = true
             $scope.userLogin = response.data.login
-            $('.inner-container').height('27%')
         }, function(error) {
             $scope.showLoader = false
             if (error.status === 400) {
