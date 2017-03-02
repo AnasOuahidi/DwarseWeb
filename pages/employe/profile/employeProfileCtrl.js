183be28ea0736e286993cb0cea9623d237dee82a
akagami <anasouahidi@gmail.com>
2017-03-02 01:25:34 +0100

    [aouahidi]: add employe profile

--- pages/employe/profile/employeProfileCtrl.js
+++ pages/employe/profile/employeProfileCtrl.js
@@ -1,4 +1,19 @@
-export let employeProfileCtrl = ['$scope', function($scope) {
-    $('title').html('profile')
+export let employeProfileCtrl = ['$scope', 'Upload', function($scope, Upload) {
+    $('title').html('Profile')
     $('.datepicker').pickadate()
+
+    $scope.sauvegarder = function (dataUrl, picName) {
+        console.log(Upload.dataUrltoBlob(dataUrl, picName))
+        // Upload.upload({
+        //     url: Factory.url('/'),
+        //     method: 'POST',
+        //     data: {
+        //         file: Upload.dataUrltoBlob(dataUrl, picName),
+        //         etudiant: $scope
+        //     },
+        // }).then(function(response) {
+        // }, function(response) {
+        // }, function(evt) {
+        // });
+    }
 }]
