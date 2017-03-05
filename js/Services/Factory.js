d99b45181616edbedb35fd8d9af787b9b5e2e0c9
akagami <anasouahidi@gmail.com>
2017-03-05 13:27:34 +0100

    [aouahidi]: ajoutde la librairie google maps

--- js/Services/Factory.js
+++ js/Services/Factory.js
@@ -2,7 +2,7 @@ export function Factory() {
     let factory = {
         token: null,
         // dns: 'http://localhost:8000',
-        dns: 'https://dwarse.herokuapp.com',
+        dns: 'http://dwarse.dev',
         jsonHerders: {
             headers: {
                 'Accept': 'application/json',
