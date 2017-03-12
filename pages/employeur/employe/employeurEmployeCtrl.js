4e324f836f4d9f657478c19114b873d7f51e904c
herfedos <gueddou.yasser@gmail.com>
2017-03-12 17:04:24 +0100

    [Yasser]: modification pages (EmployeEmployeur|AddEmployeModal)

--- pages/employeur/employe/employeurEmployeCtrl.js
+++ pages/employeur/employe/employeurEmployeCtrl.js
@@ -1,87 +1,89 @@
 export let employeurEmployeCtrl = ['$scope', 'NgTableParams', '$http', 'Factory', '$uibModal', function($scope, NgTableParams, $http, Factory, $uibModal) {
-    $('title').html('Liste employés')
-
-    $http.get(Factory.url('/employeur/employe'), null, Factory.jsonHerders).then(function(response)
-    {
-        $scope.tableauEmployes = []
-        for (let i = 0; i < response.data.length; i++) {
-            window._.forEach(response.data[i].employe, function(value, key) {
-                if (value == null) {
-                    if (key !== 'photo') {
-                        response.data[i].employe[key] = '-'
-                    }
-                }
-            })
-            let age
-            if (response.data[i].employe.dateNaissance == '-') {
-                let date = response.data[i].employe.dateNaissance.substring(0, 10)
-                let dateArray = date.split('/')
-                let day = parseInt(dateArray[0])
-                let month = parseInt(dateArray[1])
-                let year = parseInt(dateArray[2])
-                let momentDate = window.moment()
-                momentDate.set('year', year)
-                momentDate.set('month', month - 1)
-                momentDate.set('date', day)
-                age = window.moment().diff(momentDate, 'years')
-            } else {
-                age = '-'
-            }
+    $('title').html('employes')
+    let calculerAge = (datenaissance) => {
+        var jr = datenaissance.substring(2)
+        var mois = datenaissance.substring(3, 5)
+        var an = datenaissance.substring(6, 10)
+        var date = window.moment()
+        date.set('year', an)
+        date.set('month', mois - 1)
+        date.set('day', jr)
+        var age = window.moment().diff(date, 'years')
+        return age
+    }
+    $http.get(Factory.url('/employeur/employe'), null, Factory.jsonHerders).then(function(response) {
+        let employes = response.data
+        let listeEmployes = []
+        for (let i = 0; i < employes.length; i++) {
+            let datenaissance = employes[i].employe.dateNaissance
             let employe = {
-                id: response.data[i].employe.id,
-                email: response.data[i].email,
-                nom: response.data[i].employe.nom,
-                prenom: response.data[i].employe.prenom,
-                age: age,
-                numTel: response.data[i].employe.numTel,
-                categorie: "",
-                opoosed: "",
-                photo: response.data[i].employe.photo
+                id: employes[i].employe.id,
+                photo: employes[i].employe.photo ? employes[i].employe.photo : 'https://s3.amazonaws.com/dwarse/assets/img/user.png',
+                nom: employes[i].employe.nom ? employes[i].employe.nom : '-',
+                prenom: employes[i].employe.prenom ? employes[i].employe.prenom : '-',
+                email: employes[i].email ? employes[i].email : '-',
+                age: employes[i].employe.dateNaissance ? calculerAge(datenaissance) : '-',
+                telephone: employes[i].employe.numTel ? employes[i].employe.numTel : '-',
+                categorie: employes[i].employe.carte.categorie.libelle ? employes[i].employe.carte.categorie.libelle : '-',
+                etat: employes[i].employe.carte.opposed ? 'Opposée' : 'Activée',
+                etatIcon: employes[i].employe.carte.opposed
             }
-            $scope.tableauEmployes.push(employe)
+            listeEmployes.push(employe)
         }
-        $scope.tableParams = new NgTableParams({count: 20}, {counts: [], dataset: $scope.tableauEmployes})
+        $scope.tableParams = new NgTableParams({count:10}, {
+            counts: [],
+            dataset: listeEmployes
+        })
     }, function(error) {
-        console.log(error)
-    })
+        console.log(error);
+    });
 
 
     $scope.addEmploye = function() {
         let ajoutEmployeModal = $uibModal.open({
             animation: true,
             template: require('./../ajoutEmployeModal.html'),
-            size: 'md',
             scope: $scope,
-            controller: ['$scope', '$http', 'Factory', function($scope, $http, Factory) {
-                $scope.sauvegarder = function() {
+            controller: ['$scope', '$http', 'Factory', function($scope, $http, Factory) { //Controller de la fenêtre. Il doit prend en paramètre tous les élèments du "resolve".
+                $scope.valider = function() {
                     ajoutEmployeModal.close()
                     $http.post(Factory.url('/employeur/employe'), $scope.newEmploye, Factory.jsonHerders).then(function(response) {
-                        console.log(response.data)
                     }, function(error) {
                         console.log(error)
                     })
                 }
+                $scope.cancel = function() {
+                    ajoutEmployeModal.close();
+                }
             }]
         })
-    }
-    $scope.opposer = (id) => {
-        window.swal({
-            title: "Vous êtes sur de vouloir opposer votre carte?",
-            text: "",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Confirmer"
-        }, function() {
-            $http.post(Factory.url('/employeur/opposition'), {id: id}, Factory.jsonHerders).then((response) => {
-                swal(
-                    response.data.Success,
-                    'Carte opposée.',
-                    'success'
-                )
-            }, (error) => {
-                console.log(error)
-            })
-        })
-    }
+    };
+
+    $scope.opposer = function(id) {
+        swal({
+                title: 'Etes vous sur?',
+                text: "Attention, cette Action est irréversible!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: 'Oui, opposez la carte',
+                cancelButtonText: "Non, Annuler!",
+                closeOnConfirm: false,
+                closeOnCancel: true,
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    $http.post(Factory.url('/employeur/opposition'), {id: id}, Factory.jsonHerders).then(function(response){
+                        swal(
+                            'Opposée!',
+                            'Votre carte est Opposée',
+                            'success');
+                    }, function(error) {
+                        console.log(error)
+                    })
+                }
+            }
+        )}
+
 }]
+
