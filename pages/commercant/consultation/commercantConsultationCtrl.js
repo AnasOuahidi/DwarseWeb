7907b13740043ca45bfc40b24fabba71ff31263e
nbengamra <nihel.ben-gamra@insa-lyon.fr>
2017-03-14 15:49:58 +0100

    [Ben Gamra Nihel]: mise à jour page commerçant

--- pages/commercant/consultation/commercantConsultationCtrl.js
+++ pages/commercant/consultation/commercantConsultationCtrl.js
@@ -1,3 +1,36 @@
-export let commercantConsultationCtrl = ['$scope', function($scope) {
+export let commercantConsultationCtrl = ['$scope', 'Factory', '$http','NgTableParams' , function($scope, Factory, $http, NgTableParams) {
     $('title').html('consultation')
+
+    $http.get(Factory.url('/commercant/consultation/solde'), null, Factory.jsonHerders).then((response) => {
+        $scope.solde = response.data.solde
+    }, (error) => {
+        if (error.status == 400) {
+            $scope.error = ''
+        }
+
+    })
+    $http.get(Factory.url('/commercant/consultation/historique'), null, Factory.jsonHerders).then(function(response)
+        {
+            console.log(response.data)
+            $scope.tableauHistorique = []
+            for (let i = 0; i < response.data.length; i++) {
+                let dateTime = response.data[i].date
+                let historique = {
+                    id: response.data[i].id,
+                    date : dateTime.substring(0,10),
+                    time : dateTime.substring(13,21),
+                    nomEmp: response.data[i].carte.employe.nom,
+                    prenomEmp: response.data[i].carte.employe.prenom,
+                    libelleEmployeur : response.data[i].carte.employe.employeur.libelle,
+                    montant : response.data[i].montant
+
+                }
+                $scope.tableauHistorique.push(historique)
+            }
+            $scope.tableParams = new NgTableParams({count: 20}, {counts: [], dataset: $scope.tableauHistorique})
+        }, function(error) {
+            console.log(error)
+        }
+    )
 }]
+
--- pages/commercant/historique/commercantHistoriqueCtrl.js
+++ pages/commercant/historique/commercantHistoriqueCtrl.js
@@ -1,33 +1,33 @@
-export let commercantHistoriqueCtrl = ['$scope', 'Factory', '$http','NgTableParams' , function($scope, Factory, $http, NgTableParams) {
-    $('title').html('historique')
-
-    $http.get(Factory.url('/commercant/consultation/solde'), null, Factory.jsonHerders).then((response) => {
-        $scope.solde = response.data.solde
-    }, (error) => {
-        if (error.status == 400) {
-            $scope.error = ''
-        }
-
-    })
-    $http.get(Factory.url('/commercant/consultation/historique'), null, Factory.jsonHerders).then(function(response)
-        {
-            console.log(response.data)
-            $scope.tableauHistorique = []
-            for (let i = 0; i < response.data.length; i++) {
-                let historique = {
-                    id: response.data[i].id,
-                    date: response.data[i].date,
-                    nomEmp: response.data[i].carte.employe.nom,
-                    prenomEmp: response.data[i].carte.employe.prenom,
-                    libelleCom: response.data[i].lecteur.commercant.libelle,
-                    montantCarte: response.data[i].carte.solde
-
-                }
-                $scope.tableauHistorique.push(historique)
-            }
-            $scope.tableParams = new NgTableParams({count: 20}, {counts: [], dataset: $scope.tableauHistorique})
-        }, function(error) {
-            console.log(error)
-        }
-    )
-}]
+// export let commercantHistoriqueCtrl = ['$scope', 'Factory', '$http','NgTableParams' , function($scope, Factory, $http, NgTableParams) {
+//     $('title').html('historique')
+//
+//     $http.get(Factory.url('/commercant/consultation/solde'), null, Factory.jsonHerders).then((response) => {
+//         $scope.solde = response.data.solde
+//     }, (error) => {
+//         if (error.status == 400) {
+//             $scope.error = ''
+//         }
+//
+//     })
+//     $http.get(Factory.url('/commercant/consultation/historique'), null, Factory.jsonHerders).then(function(response)
+//         {
+//             console.log(response.data)
+//             $scope.tableauHistorique = []
+//             for (let i = 0; i < response.data.length; i++) {
+//                 let historique = {
+//                     id: response.data[i].id,
+//                     date: response.data[i].date,
+//                     nomEmp: response.data[i].carte.employe.nom,
+//                     prenomEmp: response.data[i].carte.employe.prenom,
+//                     libelleCom: response.data[i].lecteur.commercant.libelle,
+//                     montantCarte: response.data[i].carte.solde
+//
+//                 }
+//                 $scope.tableauHistorique.push(historique)
+//             }
+//             $scope.tableParams = new NgTableParams({count: 20}, {counts: [], dataset: $scope.tableauHistorique})
+//         }, function(error) {
+//             console.log(error)
+//         }
+//     )
+// }]
