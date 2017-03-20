56e5fc88264d99b7d61b61958fcf5191c6a4f742
anasouahidi <anasouahidi@gmail.com>
2017-03-20 04:50:22 +0100

    aouahidi: mis a jour de la consultation commercant

--- pages/commercant/consultation/commercantConsultationCtrl.js
+++ pages/commercant/consultation/commercantConsultationCtrl.js
@@ -1,53 +1,45 @@
-export let commercantConsultationCtrl = ['$scope', 'Factory', '$http','NgTableParams' , function($scope, Factory, $http, NgTableParams) {
-    $('title').html('consultation')
-    $scope.soldeAvenir = 0;
-    
-    $http.get(Factory.url('/commercant/consultation/solde'), null, Factory.jsonHerders).then((response) => {
-        $scope.solde = response.data.solde
-    }, (error) => {
-        if (error.status == 400) {
-            $scope.error = ''
-        }
+export let commercantConsultationCtrl = ['$scope', 'Factory', '$http', 'NgTableParams', function ($scope, Factory, $http, NgTableParams) {
+	$('title').html('consultation')
+	$scope.soldeAvenir = 0
 
-    })
-    $http.get(Factory.url('/commercant/consultation/historique'), null, Factory.jsonHerders).then(function(response)
-        {
-            $scope.tableauHistorique = []
-            let checkDailyTransaction = (date) => {
-                    $scope.dateJr = moment().format('DD/MM/YYYY');
-                    if($scope.dateJr == date){
-                        return true
-                    }else{
-                        return false
-                    }
-                }
+	$http.get(Factory.url('/commercant/consultation/solde'), null, Factory.jsonHerders).then((response) => {
+		$scope.solde = response.data.solde
+	}, (error) => {
+		if (error.status == 400) {
+			$scope.error = ''
+		}
 
-            for (let i = 0; i < response.data.length; i++) {
-                let dateTime = response.data[i].date
-                let date = dateTime.substring(0,10)
-                let historique = {
-                    id: response.data[i].id,
-                    date : date,
-                    time : dateTime.substring(13,21),
-                    nomEmp: response.data[i].carte.employe.nom,
-                    prenomEmp: response.data[i].carte.employe.prenom,
-                    libelleEmployeur : response.data[i].carte.employe.employeur.libelle,
-                    montant : response.data[i].montant,
-                    etatTransaction : checkDailyTransaction(date) ? 'A venir' : 'Terminé',
-                    etat : checkDailyTransaction(date),
-                }       
-                $scope.tableauHistorique.push(historique)
-                    if($scope.tableauHistorique[i].date == $scope.dateJr)
-                    {
-                        $scope.soldeAvenir += $scope.tableauHistorique[i].montant;
-                    }
-            }        
+	})
+	$http.get(Factory.url('/commercant/consultation/historique'), null, Factory.jsonHerders).then(function (response) {
+			$scope.tableauHistorique = []
+			for (let i = 0; i < response.data.length; i++) {
+				let dateTime = response.data[i].date
+				let date = dateTime.substring(0, 10)
+				let historique = {
+					id: response.data[i].id,
+					date: date,
+					time: dateTime.substring(13, 21),
+					nomEmp: response.data[i].carte.employe.nom,
+					prenomEmp: response.data[i].carte.employe.prenom,
+					libelleEmployeur: response.data[i].carte.employe.employeur.libelle,
+					montant: response.data[i].montant,
+					etatTransaction: response.data[i].avenir ? 'A venir' : 'Terminé',
+					etat: response.data[i].avenir
+				}
+				$scope.tableauHistorique.push(historique)
+				if ($scope.tableauHistorique[i].etat) {
+					$scope.soldeAvenir += $scope.tableauHistorique[i].montant
+				}
+			}
 
-            $scope.tableParams = new NgTableParams({sorting: { date: "desc" }}, {counts: [], dataset: $scope.tableauHistorique})
-                    $("table").next().addClass( "text-center" )
-        }, function(error) {
-            console.log(error)
-        }
-    )
+			$scope.tableParams = new NgTableParams({sorting: {date: "desc"}}, {
+				counts: [],
+				dataset: $scope.tableauHistorique
+			})
+			$("table").next().addClass("text-center")
+		}, function (error) {
+			console.log(error)
+		}
+	)
 }]
 
